0c1bd067db973058ce0f63d46a07a4c2e5c4c818
DanielOneSinger <yaoydong@wizlawgic.com>
2024-09-24 10:11:38 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,18 +1,20 @@
 import json
 import os
-import pickle
 from zipfile import ZipFile
 import pandas as pd
+import numpy as np
 from sklearn.kernel_ridge import KernelRidge
-from sklearn.linear_model import BayesianRidge, LinearRegression
+from sklearn.linear_model import BayesianRidge, LinearRegression, ElasticNet, Lasso, Ridge
 from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.preprocessing import StandardScaler
 from updater import download_binance_daily_data, download_binance_current_day_data, download_coingecko_data, download_coingecko_current_day_data
-from config import data_base_path, model_file_path, TOKEN, MODEL, CG_API_KEY
+from config import data_base_path, model_file_path, TOKENS, MODEL, CG_API_KEY
 
 
 binance_data_path = os.path.join(data_base_path, "binance")
 coingecko_data_path = os.path.join(data_base_path, "coingecko")
-training_price_data_path = os.path.join(data_base_path, "price_data.csv")
+training_price_data_path = os.path.join(data_base_path, "price_data")
 
 
 def download_data_binance(token, training_days, region):
@@ -33,85 +35,78 @@ def download_data(token, training_days, region, data_provider):
         return download_data_binance(token, training_days, region)
     else:
         raise ValueError("Unsupported data provider")
-    
-def format_data(files, data_provider):
-    if not files:
-        print("Already up to date")
-        return
-    
-    if data_provider == "binance":
-        files = sorted([x for x in os.listdir(binance_data_path) if x.startswith(f"{TOKEN}USDT")])
-    elif data_provider == "coingecko":
-        files = sorted([x for x in os.listdir(coingecko_data_path) if x.endswith(".json")])
 
-    # No files to process
-    if len(files) == 0:
-        return
-
+def format_data(files, data_provider, token):
     price_df = pd.DataFrame()
-    if data_provider == "binance":
-        for file in files:
-            zip_file_path = os.path.join(binance_data_path, file)
+    for file in files:
+        df = pd.read_csv(file, header=None)  # 读取文件时不使用标题行
+        price_df = pd.concat([price_df, df])
 
-            if not zip_file_path.endswith(".zip"):
-                continue
+    # 设置列名
+    print('before',price_df.columns)
+    column_names = [
+        'open_time', 'open', 'high', 'low', 'close', 'volume',
+        'close_time', 'quote_asset_volume', 'number_of_trades',
+        'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'
+    ]
+    price_df.columns = column_names
 
-            myzip = ZipFile(zip_file_path)
-            with myzip.open(myzip.filelist[0]) as f:
-                line = f.readline()
-                header = 0 if line.decode("utf-8").startswith("open_time") else None
-            df = pd.read_csv(myzip.open(myzip.filelist[0]), header=header).iloc[:, :11]
-            df.columns = [
-                "start_time",
-                "open",
-                "high",
-                "low",
-                "close",
-                "volume",
-                "end_time",
-                "volume_usd",
-                "n_trades",
-                "taker_volume",
-                "taker_volume_usd",
-            ]
-            df.index = [pd.Timestamp(x + 1, unit="ms").to_datetime64() for x in df["end_time"]]
-            df.index.name = "date"
-            price_df = pd.concat([price_df, df])
-
-            price_df.sort_index().to_csv(training_price_data_path)
-    elif data_provider == "coingecko":
-        for file in files:
-            with open(os.path.join(coingecko_data_path, file), "r") as f:
-                data = json.load(f)
-                df = pd.DataFrame(data)
-                df.columns = [
-                    "timestamp",
-                    "open",
-                    "high",
-                    "low",
-                    "close"
-                ]
-                df["date"] = pd.to_datetime(df["timestamp"], unit="ms")
-                df.drop(columns=["timestamp"], inplace=True)
-                df.set_index("date", inplace=True)
-                price_df = pd.concat([price_df, df])
-
-            price_df.sort_index().to_csv(training_price_data_path)
-
+    # 添加 date 列
+    price_df['date'] = pd.to_datetime(price_df['open_time'], unit='ms')
+    print('after',price_df.columns)
+    # 保存处理后的数据
+    output_file = f"{training_price_data_path}_{token}.csv"
+    price_df.to_csv(output_file, index=False)
+    print(f"Formatted data saved for token: {token}")
+    print("Formatted data head:")
+    print(price_df.head())
+    print("\nFormatted data description:")
+    print(price_df.describe())
 
 def load_frame(frame, timeframe):
-    print(f"Loading data...")
-    df = frame.loc[:,['open','high','low','close']].dropna()
-    df[['open','high','low','close']] = df[['open','high','low','close']].apply(pd.to_numeric)
-    df['date'] = frame['date'].apply(pd.to_datetime)
+    print("Loading data...")
+    print(frame.columns)
+    
+    required_columns = ['open', 'high', 'low', 'close', 'date']
+    for col in required_columns:
+        if col not in frame.columns:
+            raise KeyError(f"Required column '{col}' not found in the data.")
+    
+    df = frame[required_columns].copy()
+    df[['open', 'high', 'low', 'close']] = df[['open', 'high', 'low', 'close']].apply(pd.to_numeric)
+    df['date'] = pd.to_datetime(df['date'])
     df.set_index('date', inplace=True)
     df.sort_index(inplace=True)
+    
+    # 重采样数据
+    df = df.resample(f'{timeframe}', label='right', closed='right', origin='end').mean()
+    
+    print(df.head())
+    return df
 
-    return df.resample(f'{timeframe}', label='right', closed='right', origin='end').mean()
+def get_model(model_name):
+    models = {
+        "LinearRegression": LinearRegression(),
+        "SVR": SVR(),
+        "KernelRidge": KernelRidge(),
+        "BayesianRidge": BayesianRidge(),
+        "RandomForestRegressor": RandomForestRegressor(),
+        "GradientBoostingRegressor": GradientBoostingRegressor(),
+        "ElasticNet": ElasticNet(max_iter=10000, tol=1e-4),  # Increased iterations and lowered tolerance
+        "Lasso": Lasso(),
+        "Ridge": Ridge()
+    }
+    return models.get(model_name)
 
-def train_model(timeframe):
-    # Load the price data
-    price_data = pd.read_csv(training_price_data_path)
+
+def train_model(timeframe, model_name, token):
+    file_path = f"{training_price_data_path}_{token}.csv"
+    if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+        raise FileNotFoundError(f"File not found or empty: {file_path}")
+    
+    price_data = pd.read_csv(file_path)
+    print(price_data.columns)
+    print('xxxxx')
     df = load_frame(price_data, timeframe)
 
     print(df.tail())
@@ -121,46 +116,37 @@ def train_model(timeframe):
 
     print(f"Training data shape: {X_train.shape}, {y_train.shape}")
 
-    # Define the model
-    if MODEL == "LinearRegression":
-        model = LinearRegression()
-    elif MODEL == "SVR":
-        model = SVR()
-    elif MODEL == "KernelRidge":
-        model = KernelRidge()
-    elif MODEL == "BayesianRidge":
-        model = BayesianRidge()
-    # Add more models here
-    else:
-        raise ValueError("Unsupported model")
-    
+    # Feature scaling
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+
+    # Get the specified model
+    model = get_model(model_name)
+    if model is None:
+        raise ValueError(f"Unsupported model: {model_name}")
+
+    print(f"Training model: {model_name} with data shape: {X_train_scaled.shape}")  # 添加调试输出
+
     # Train the model
-    model.fit(X_train, y_train)
+    model.fit(X_train_scaled, y_train)
 
-    # create the model's parent directory if it doesn't exist
-    os.makedirs(os.path.dirname(model_file_path), exist_ok=True)
-
-    # Save the trained model to a file
-    with open(model_file_path, "wb") as f:
-        pickle.dump(model, f)
-
-    print(f"Trained model saved to {model_file_path}")
+    return model, scaler
 
 
-def get_inference(token, timeframe, region, data_provider):
-    """Load model and predict current price."""
-    with open(model_file_path, "rb") as f:
-        loaded_model = pickle.load(f)
-
+def get_inference(token, timeframe, region, data_provider, model, scaler):
+    """Predict current price using the provided model."""
     # Get current price
     if data_provider == "coingecko":
         X_new = load_frame(download_coingecko_current_day_data(token, CG_API_KEY), timeframe)
     else:
-        X_new = load_frame(download_binance_current_day_data(f"{TOKEN}USDT", region), timeframe)
-    
+        X_new = load_frame(download_binance_current_day_data(f"{token}USDT", region), timeframe)
+
     print(X_new.tail())
     print(X_new.shape)
 
-    current_price_pred = loaded_model.predict(X_new)
+    # Scale the new data
+    X_new_scaled = scaler.transform(X_new)
+
+    current_price_pred = model.predict(X_new_scaled)
 
     return current_price_pred[0]
